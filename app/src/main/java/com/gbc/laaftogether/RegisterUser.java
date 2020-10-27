e40dc6d7832908a309f9eebe08262f9f43b5d9b4
Dimpu <dimplejayswal1018@gmail.com>
2020-10-27 12:23:11 -0400

    laaftogether commit

---
+++ app/src/main/java/com/gbc/laaftogether/RegisterUser.java
@@ -0,0 +1,118 @@
+package com.gbc.laaftogether;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.gbc.laaftogether.LoginActivity;
+import com.gbc.laaftogether.MainActivity;
+import com.gbc.laaftogether.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterUser extends AppCompatActivity{
+
+    EditText fullName,email,password,phone;
+    Button registerBtn,goToLogin;
+    boolean valid = true;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+        // Initialize Firebase Auth
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        fullName = findViewById(R.id.registerName);
+        email = findViewById(R.id.registerEmail);
+        password = findViewById(R.id.registerPassword);
+        phone = findViewById(R.id.registerPhone);
+        registerBtn = findViewById(R.id.registerBtn);
+        goToLogin = findViewById(R.id.gotoLogin);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkField(fullName);
+                checkField(email);
+                checkField(password);
+                checkField(phone);
+
+                if (valid){
+                    // start the user registration
+                    fAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            Toast.makeText(RegisterUser.this, "Account Created", Toast.LENGTH_SHORT).show();
+
+                            DocumentReference df = fStore.collection("Users").document(user.getUid());
+                            //restore the data
+                            Map<String,Object> userInfo = new HashMap<>();
+                            userInfo.put("FullName",fullName.getText().toString());
+                            userInfo.put("UserEmail", email.getText().toString());
+                            userInfo.put("PhoneNumber", phone.getText().toString());
+                            //specify if the uesr is admin
+                            userInfo.put("isUser","1");
+                            // userInfo.put("isAdmin")
+                            df.set(userInfo);
+
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(RegisterUser.this, "Failed to create account", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+            }
+        });
+
+        goToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+
+
+
+    }
+
+    public boolean checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+        return valid;
+
+
+
+    }
+}
